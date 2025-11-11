7a235f22c189b9b1b4df26e109fe39b1f91b953f
dandodado <jo4326892@gmail.com>
2025-11-11 11:53:45 +0900

    클래스 세부 리팩토링

--- ParticleSystem.js
+++ ParticleSystem.js
@@ -1,20 +1,42 @@
+class ParticleSystem {
+  constructor(position) {
+    this.origin = position.copy();
+    this.particles = [];
+  }
 
-// 파티클 시스템 클래스
-let ParticleSystem = function(position) {
-  this.origin = position.copy();
-  this.particles = [];
-};
+  addParticle() {
+    this.particles.push(new Particle(this.origin));
+  }
 
-ParticleSystem.prototype.addParticle = function() {
-  this.particles.push(new Particle(this.origin));
-};
-
-ParticleSystem.prototype.run = function() {
-  for (let i = this.particles.length-1; i >= 0; i--) {
-    let p = this.particles[i];
-    p.run();
-    if (p.isDead()) {
-      this.particles.splice(i, 1);
+  run() {
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      let p = this.particles[i];
+      p.run();
+      if (p.isDead()) {
+        this.particles.splice(i, 1);
+      }
     }
   }
-};
+}
+
+
+
+// // 파티클 시스템 클래스
+// let ParticleSystem = function(position) {
+//   this.origin = position.copy();
+//   this.particles = [];
+// };
+
+// ParticleSystem.prototype.addParticle = function() {
+//   this.particles.push(new Particle(this.origin));
+// };
+
+// ParticleSystem.prototype.run = function() {
+//   for (let i = this.particles.length-1; i >= 0; i--) {
+//     let p = this.particles[i];
+//     p.run();
+//     if (p.isDead()) {
+//       this.particles.splice(i, 1);
+//     }
+//   }
+// };
