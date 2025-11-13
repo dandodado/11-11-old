7b7dee1256042f27c82d495204acb907dcd66b8e
dandodado <jo4326892@gmail.com>
2025-11-13 17:36:01 +0900

    인덱스 수정

--- ParticleSystem.js
+++ ParticleSystem.js
@@ -19,24 +19,3 @@ class ParticleSystem {
   }
 }
 
-
-
-// // 파티클 시스템 클래스
-// let ParticleSystem = function(position) {
-//   this.origin = position.copy();
-//   this.particles = [];
-// };
-
-// ParticleSystem.prototype.addParticle = function() {
-//   this.particles.push(new Particle(this.origin));
-// };
-
-// ParticleSystem.prototype.run = function() {
-//   for (let i = this.particles.length-1; i >= 0; i--) {
-//     let p = this.particles[i];
-//     p.run();
-//     if (p.isDead()) {
-//       this.particles.splice(i, 1);
-//     }
-//   }
-// };
